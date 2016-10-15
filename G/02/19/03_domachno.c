0f75cf7aa18b07695b52baa8f4888518d6ecdc77
mimibeleva <mimibelev@gmail.com>
2016-10-15 19:11:53 +0300

    Update 03_domachno.c

--- G/02/19/03_domachno.c
+++ G/02/19/03_domachno.c
@@ -2,13 +2,14 @@
 int main()
 {   int a;
     scanf("%d",&a);
-    if(a==1)printf("\n one");
-    if(a==2)printf("\n two");
-    if(a==3)printf("\n three");
-    if(a==4)printf("\n four");
-    if(a==5)printf("\n five");
-    if(a==6)printf("\n six");
-    if(a==7)printf("\n seven");
-    if(a==8)printf("\n eight");
-    if(a==9)printf("\n nine");
+    if(a==0)printf("\nzero");
+    if(a==1)printf("\none");
+    if(a==2)printf("\ntwo");
+    if(a==3)printf("\nthree");
+    if(a==4)printf("\nfour");
+    if(a==5)printf("\nfive");
+    if(a==6)printf("\nsix");
+    if(a==7)printf("\nseven");
+    if(a==8)printf("\neight");
+    if(a==9)printf("\nnine");
 }
