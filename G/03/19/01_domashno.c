3f4310d61601ce65919c2ce04d4ef94d249a4142
mimibeleva <mimibelev@gmail.com>
2016-10-22 10:06:32 +0300

    Update 01_domashno.c

--- G/03/19/01_domashno.c
+++ G/03/19/01_domashno.c
@@ -6,7 +6,8 @@ scanf("%d",&n);
 for(s=0;i<n;i++){scanf("%d",&m);
                   s=s+m;}
 a=(float)s/n;
-printf("\nSum:");printf("%d",s);
-printf("\nAvg:");printf("%.2f",a);
+printf("\nSum:%d",s);
+printf("\nAvg:%.2f",a);
+return 0;
 }
 
