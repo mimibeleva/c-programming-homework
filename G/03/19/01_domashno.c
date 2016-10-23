013286af07c3dc6f517685dc8d9b4262235212df
mimibeleva <mimibelev@gmail.com>
2016-10-23 09:04:03 +0300

    Update 01_domashno.c

--- G/03/19/01_domashno.c
+++ G/03/19/01_domashno.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-int n,m,i=0,s;
+int n,m,i,s=0;
 float a;
 scanf("%d",&n);
-for(s=0;i<n;i++){scanf("%d",&m);
+for(i=0;i<n;i++){scanf("%d",&m);
                   s=s+m;}
 a=(float)s/n;
 printf("\nSum:%d",s);
